8b7b9bff4e7f9b493f2f28e314b6081b1189ecfd
wilku1211 <wilku1211c@gmail.com>
2019-01-30 10:48:56 +0100

    g

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Router from "next/router";
+
    
 const Blog = ( props ) => {
    
 return (
-   <div><h1>Strona Blog</h1></div>
+   <div><h1>Strona Blog</h1>
+    <span className="display-4" onClick={() => Router.back()}>
+                back
+              </span>
+   
+   
+   </div>
+   
 )
 };
    
